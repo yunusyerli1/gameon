8dea5e1c1631d5d4c694962ab826a5f87299d9f6
yunusyerli1 <yunus.yerli@gaboras.com>
2022-08-28 11:33:53 +0300

    formik and yup installed

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { login } from "../firebase.js"
+
 export default function Login() {
 
     const [ email, setEmail ] = useState('');
